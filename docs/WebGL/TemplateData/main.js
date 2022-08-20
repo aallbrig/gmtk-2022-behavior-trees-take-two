a5f3d4b99aa437b592e2818d996d7eba8d913b58
aallbrig <aallbrig@gmail.com>
2022-08-20 17:06:27 +0000

    Deploying to github-pages from @ aallbrig/gmtk-2022-behavior-trees-take-two@76b1771d4e866c20469c8e71fa72a5088d226258 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "gmtk-2022-behavior-trees-take-two",
-    productVersion: "0.0.46",
+    productVersion: "0.0.54",
     devicePixelRatio: 1,
 }
 
