daf56928c6dc1ec238e329bc4717eaed978ad041
Andrew Allbright <aallbrig@gmail.com>
2022-08-24 17:35:49 -0400

    Assert test volume expectations

--- unity/gmtk-2022-behavior-trees-take-two/Assets/Tests/PlayMode/Scenarios/ForMasterChief/MasterChiefAndCamera.cs
+++ unity/gmtk-2022-behavior-trees-take-two/Assets/Tests/PlayMode/Scenarios/ForMasterChief/MasterChiefAndCamera.cs
@@ -36,21 +36,15 @@ namespace Tests.PlayMode.Scenarios.ForMasterChief
                 Object.Destroy(gameObject);
             _destroyMeAtEnd.Clear();
         }
-        [Test]
-        public void MasterChiefAndCameraSimplePasses()
-        {
-            // Use the Assert class to test conditions.
-            
-        }
-
-        // A UnityTest behaves like a coroutine in PlayMode
-        // and allows you to yield null to skip a frame in EditMode
         [UnityTest]
-        public IEnumerator MasterChiefAndCameraWithEnumeratorPasses()
+        public IEnumerator MasterChiefAndCamera_Exist()
         {
             // Use the Assert class to test conditions.
             // yield to skip a frame
             yield return null;
+            Assert.NotNull(_sutPrefabInstance);
+            Assert.NotNull(_testCameraInstance);
+            Assert.NotNull(_testPlatform);
         }
     }
 }
